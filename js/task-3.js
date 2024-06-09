197ee1b48faba47f47cc6704ef6a9fd88c019c98
DmytroPryadko <160414230+DmytroPryadko@users.noreply.github.com>
2024-06-09 13:06:13 +0200

    11

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,6 @@
-function getElementWidth(content, padding, border) {
-    return Number.parseFloat(content)  + Number.parseFloat(padding) * 2  + Number.parseFloat(border) * 2 
-}
-
-getElementWidth("50px", "8px", "4px");
-getElementWidth("60px", "12px", "8.5px");
-getElementWidth("200px", "0px", "0px");
-
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+const nameInput = document.querySelector("#name-input");
+const output = document.querySelector("#name-output");
+nameInput.addEventListener("input", (e) => {
+   let trueOutput = nameInput.value.trim();
+   output.textContent = !trueOutput ?  "Anonymous" : trueOutput
+});
