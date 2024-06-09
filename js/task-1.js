acfe61c913adca85ebd95ad21a8337fb81ef8fd4
DmytroPryadko <160414230+DmytroPryadko@users.noreply.github.com>
2024-06-09 13:04:24 +0200

    1

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,11 @@
-function makeTransaction(quantity, pricePerDroid){
-return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`
-}
-makeTransaction(5, 3000); 
-makeTransaction(3, 1000);
-makeTransaction(10, 500);
-
-console.log(makeTransaction(5, 3000));
-console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500));
+const ulCategories = document.querySelectorAll('#categories .item');
+const total = ulCategories.length;
+console.log(ulCategories);
+console.log(`Number of categories: ${total}`);
+ulCategories.forEach(category => {
+    const categoryName = category.querySelector('h2').textContent;
+    // const categoryItemsCount = category.querySelectorAll('ul li').length;
+    const categoryItemsCount = category.querySelector('ul').childElementCount 
+    console.log(`Category: ${categoryName}`);
+    console.log(`Elements: ${categoryItemsCount}`);
+});
